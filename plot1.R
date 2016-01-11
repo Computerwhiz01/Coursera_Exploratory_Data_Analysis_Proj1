8cf8f3b3f9362381109e9dcbb7e3ea0023cfdfe7
Computerwhiz01 <djpfeiffer@hotmail.com>
2016-01-10 16:54:26 -0800

    Submit files for Project 1
    Coursera Exploratory Data Analysis Project 1

---
+++ plot1.R
@@ -0,0 +1,22 @@
+## Coursera Exploratory Data Analysis - Project 1
+## Plot 1
+
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+## Subset data for (2/1/2007 - 2/2/2007) 2 days only
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Convert dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 1
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Save to file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
